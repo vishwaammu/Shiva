a4fa9379fa759f9691df45f695fc89f0cbcc5a15
vishwaammu <141515398+vishwaammu@users.noreply.github.com>
2024-10-19 00:21:29 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
-const parent = React.creatElement(
-    "div", 
-    {id: "parent"}, 
-   [ React.createElement(
-    "div",
-    {id: "child"},[
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-    
-]);,
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-React.createElement(
-    "div",
-    {id: "child2"},[
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-    
-]),
-]);
+const Header = () => {
+    return (
+    <div className = "header">
+       
+            <div className = "logo-container">
+                <img className="Logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>Abouit Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+    </div>
+    );
+};
 
-//JSX
-console.log(parent); //object
+const AppLayout = () => {
+    return (
+        <div className="app">,
+            <Header />
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<AppLayout/>);
